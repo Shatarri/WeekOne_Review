30218a6e3ac28683fa96226efcf82dc13a80e82c
Diamond Dale <dale.diamond@aol.com>
2024-09-30 11:09:04 -0500

    created addition method

--- src/main/java/com/ps/Calculator.java
+++ src/main/java/com/ps/Calculator.java
@@ -2,15 +2,23 @@ package com.ps;
 
 class Calculator {
     public static void main(String[] args){
+        int sum = addition(200, 300);
+        System.out.println(sum);
     }
     //Creating different static methods
     //static subtract method
-    public static void subtractStatic(int num1 - int num2){
+    public static int subtract(int number1 , int number2){
+        return number1 - number2;
 
 
     }
-
+    //static add method
+    public static int addition(int num1 , int num2){
+        return num1 + num2;
+    }
     //static multiplication method
+
+
     //static division method
     //receive two inputs (ints)
     //return an int
