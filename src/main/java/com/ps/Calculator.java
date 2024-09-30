dc6ec182ce31514d584de5ceab89341e5fbf2ed2
Diamond Dale <dale.diamond@aol.com>
2024-09-30 10:51:24 -0500

    first commit

---
+++ src/main/java/com/ps/Calculator.java
@@ -0,0 +1,18 @@
+package com.ps;
+
+class Calculator {
+    public static void main(String[] args){
+    }
+    //Creating different static methods
+    //static subtract method
+    public static void subtractStatic(int num1 - int num2){
+
+
+    }
+
+    //static multiplication method
+    //static division method
+    //receive two inputs (ints)
+    //return an int
+
+}
